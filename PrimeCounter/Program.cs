b7c6d67f43c9bf9295940a28161114df0756805d
Jeremy Grenell <jhgrenell412@gmail.com>
2020-03-04 09:35:13 -0800

    Added comments explaining thought process throughout some resolutions.

--- PrimeCounter/Program.cs
+++ PrimeCounter/Program.cs
@@ -1,30 +1,33 @@
 ﻿using System;
 
+// Given an integer, I need to return the sum of that number of primes going in ascending order.
+// Example: Given the integer 5, I will need to calculate the sum of 2, 3, 5, 7, and 11, and the return should be 28.
+
 namespace PrimeCounter
 {
     class Program
     {
+        // First, I will need to create a method to determine whether or not the numbers being encountered are primes.
         public static bool isPrime(int num)
         {
-            if (num < 2) return false;
-            for (int i = 2; i < num; ++i)
+            if (num < 2) return false; // This if statement refutes the number 1.
+            for (int i = 2; i < num; ++i) // I am using a for loop to evaluate each number given to it.
             {
-                if (num % i == 0) return false;
+                if (num % i == 0) return false; // I am diving things by 2, if there is no remainder, then the number is not a prime.
             }
             return true;
         }
         public static int GetPrimeSum(int n)
         {
-            int primeSum = 0, primeCounter = 2;
-            int numberOfPrimes = 0;
-            while (numberOfPrimes < n)
+            int primeSum = 0, counter = 0, numberOfPrimes = 0;
+            while (numberOfPrimes < n) // This loop ensures that I sum the number of primes given.
             {
-                if (isPrime(primeCounter))
+                if (isPrime(counter)) // This uses the method I created above.
                 {
-                    primeSum += primeCounter;
-                    numberOfPrimes++;
+                    primeSum += counter;
+                    numberOfPrimes++; // If the counter reaches a prime, it is added to the number of primes, as well as the sum.
                 }
-                primeCounter++;
+                counter++; // If the number being evaluated is not a prime, it simply moves on to the next number.
             }
             return primeSum;
         }
